e847505eed8d7219b2dc797bf5215a5ca84781d0
Martin Kierkegaard <mark@easj.dk>
2017-09-18 14:00:39 +0200

    tilføjet dll og renamet service og interface fjernet unødvendige metoder

--- WcfKaffeService1/KaffeService1.svc.cs
+++ WcfKaffeService1/KaffeService1.svc.cs
@@ -5,29 +5,26 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using kaffe;
 
 namespace WcfKaffeService1
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    public class KaffeService1 : IKaffe
     {
-        public string GetData(int value)
+
+        public int GetSortKaffePris()
         {
-            return string.Format("You entered: {0}", value);
+            var sortkaffe = new SortKaffe();
+
+            return sortkaffe.Pris();
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        public string GetSortKaffeStyrke()
         {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            var sortkaffe = new SortKaffe();
+            return sortkaffe.Styrke();
         }
     }
 }
